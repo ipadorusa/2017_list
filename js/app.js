6a02dde3948f61b1794ae89b11cedcd6021fa843
ipadorusa <rainguy1@naver.com>
2016-12-05 21:39:14 +0900

    ^^

--- js/app.js
+++ js/app.js
@@ -5,7 +5,7 @@
 angular.module('myApp', ['myApp.filters', 'myApp.services', 'myApp.directives']).
   config(['$routeProvider', function($routeProvider) {
     $routeProvider.when('/intro', {templateUrl: 'partials/intro.html', controller: GenericViewCtrl});
-    $routeProvider.when('/smartdashboard', {templateUrl: 'partials/smartdashboard.html', controller: GenericViewCtrl});
+    $routeProvider.when('/codelist', {templateUrl: 'partials/codelist.html', controller: GenericViewCtrl});
     $routeProvider.when('/edashboard', {templateUrl: 'partials/edashboard.html', controller: GenericViewCtrl});
     $routeProvider.when('/pharosjava', {templateUrl: 'partials/pharosjava.html', controller: GenericViewCtrl});
     $routeProvider.otherwise({redirectTo: '/intro'});
